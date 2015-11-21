a03a99726dcca9a815ad71bcb08f4ff359de803a
rasbt <mail@sebastianraschka.com>
2015-11-20 19:10:38 -0500

    set up setup.py

---
+++ setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup
+
+setup(name='biopandas',
+      version='0.1.0',
+      description='Molecular Structures in Pandas DataFrames',
+      author='Sebastian Raschka',
+      author_email='mail@sebastianraschka.com',
+      url='https://github.com/rasbt/biopandas',
+      packages=['biopandas'],
+      data_files = [('', ['LICENSE']),
+                    ('', ['README.rst']),
+                   ],
+      license='BSD3',
+      platforms='any',
+      classifiers=[
+             'License :: OSI Approved :: BSD License',
+             'Development Status :: 5 - Production/Stable',
+             'Operating System :: Microsoft :: Windows',
+             'Operating System :: POSIX',
+             'Operating System :: Unix',
+             'Operating System :: MacOS',
+             'Programming Language :: Python :: 2',
+             'Programming Language :: Python :: 2.7',
+             'Programming Language :: Python :: 3',
+             'Programming Language :: Python :: 3.3',
+             'Programming Language :: Python :: 3.4',
+             'Programming Language :: Python :: 3.5',
+             'Topic :: Scientific/Engineering',
+      ],
+      long_description="""
+Biopandas is a python package for working with molecular structures
+in pandas DataFrames.
+
+
+Contact
+=============
+
+If you have any questions or comments about biopandas, please feel free to contact me via
+eMail: mail@sebastianraschka.com
+or Twitter: https://twitter.com/rasbt
+
+This project is hosted at https://github.com/rasbt/biopandas
+
+The documentation can be found at http://rasbt.github.io/biopandas/
+
+""",
+    )
