d97595e02aa4f0cc7898a3439d2844476a7a4c0f
rasbt <mail@sebastianraschka.com>
2017-05-26 14:43:16 -0400

    style fixes

--- biopandas/testutils/testutils.py
+++ biopandas/testutils/testutils.py
@@ -28,10 +28,10 @@ def assert_raises(exception_type, message, func, *args, **kwargs):
     exception_type : exception
         The exception that should be raised
     message : str (default: None)
-        The error message that should be raised. Ignored if False or None.
+        The error message that should be raised. Ignored if False or None
     func : callable
         The function that raises the exception
-    *args : positional arguments to `func`.
+    *args : positional arguments to `func`
     **kwargs : keyword arguments to `func`
 
     """
