01abc711f4ad342009ef4e92d3def815f3232c72
rasbt <mail@sebastianraschka.com>
2017-09-27 00:11:28 -0400

    add unittest for deprecated string arg

--- biopandas/pdb/tests/test_distance.py
+++ biopandas/pdb/tests/test_distance.py
@@ -22,6 +22,20 @@ def test_equal():
     assert dist[dist < 3].all() == expect.all()
 
 
+def test_deprecated_str_arg():
+    TESTDATA_1t48 = os.path.join(os.path.dirname(__file__), 'data',
+                                                            '1t48_995.pdb')
+
+    p1t48 = PandasPdb()
+    p1t48.read_pdb(TESTDATA_1t48)
+    dist = p1t48.distance(xyz=(70.785, 15.477, 23.359), records='ATOM')
+
+    expect = pd.Series([2.533259, 1.520502, 0.000000, 1.257597, 1.252510],
+                       index=[12, 13, 14, 15, 16])
+    assert dist[dist < 3].all() == expect.all()
+
+
+
 def test_use_external_df():
     TESTDATA_1t48 = os.path.join(os.path.dirname(__file__), 'data',
                                                             '1t48_995.pdb')
