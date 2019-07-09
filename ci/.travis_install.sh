118a67f851823421afd6f4e6e22f7b8665a917c2
rasbt <mail@sebastianraschka.com>
2019-07-09 22:00:54 +0200

    use more recent conda installer

--- ci/.travis_install.sh
+++ ci/.travis_install.sh
@@ -19,8 +19,7 @@ deactivate
 
 # Use the miniconda installer for faster download / install of conda
 # itself
-wget http://repo.continuum.io/miniconda/Miniconda-3.9.1-Linux-x86_64.sh \
-    -O miniconda.sh
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 chmod +x miniconda.sh && ./miniconda.sh -b
 export PATH=/home/travis/miniconda/bin:$PATH
 conda update --yes conda
