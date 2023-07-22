8cb26e3d1df37a467d9c188ef1e0d9ee99782b51
AbdulHamid Merii <ah_merii@outlook.com>
2023-07-22 22:54:37 +0100

    Add extra requirements for testing in setup.py

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setup(name='biopandas',
                     },
       include_package_data=True,
       install_requires=install_reqs,
+      extras_require={'test': ['pytest', 'pytest-cov'],}
       license='BSD 3-Clause',
       platforms='any',
       classifiers=[
