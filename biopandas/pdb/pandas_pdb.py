9d0de4f73ab28602f825732fe72fae50ac2ccffe
Arian Jamasb <arjamasb@gmail.com>
2023-07-31 13:58:35 -0400

    make column subsetting more robust

--- biopandas/pdb/pandas_pdb.py
+++ biopandas/pdb/pandas_pdb.py
@@ -611,7 +611,10 @@ class PandasPdb(object):
         dfs = {r: self.df[r].copy() for r in records if not self.df[r].empty}
 
         # Drop unexpected columns
-        dfs = {k: v[list(pdb_df_columns)] for k, v in dfs.items() if k in {"ATOM", "HETATM"}}
+        for k, v in dfs.items():
+            if k in {"ATOM", "HETATM"}}:
+                overlap_columns = set(pdb_df_columns).intersection(set(df.columns))
+                dfs[k] = v[list(overlap_columns)]
 
         for r in dfs:
             for col in pdb_records[r]:
