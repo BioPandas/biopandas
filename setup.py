82f39b32e438807d7f11c208dbd97d9e93fa22e1
Arian Jamasb <arjamasb@gmail.com>
2023-08-28 16:35:31 +0200

    add missing nose test requirement

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(name='biopandas',
                     },
       include_package_data=True,
       install_requires=install_reqs,
-      extras_require={'test': ['pytest', 'pytest-cov','flake8'],},
+      extras_require={'test': ['pytest', 'pytest-cov','flake8', 'nose'],},
       license='BSD 3-Clause',
       platforms='any',
       classifiers=[
