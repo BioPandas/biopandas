cf3a8e8a1c2706eccb8c7d2452a60e9d5a935760
kierandidi <kieran.didi@gmail.com>
2024-07-08 14:02:57 +0200

    [feat] added tests for multiple models

---
+++ biopandas/mmcif/tests/test_multiple_models.py
@@ -0,0 +1,26 @@
+# BioPandas
+# Author: Sebastian Raschka <mail@sebastianraschka.com>
+# Author: Arian Jamasb <arian@jamasb.io>
+# License: BSD 3 clause
+# Project Website: http://rasbt.github.io/biopandas/
+# Code Repository: https://github.com/rasbt/biopandas
+import os
+
+from biopandas.mmcif import PandasMmcif
+
+TESTDATA_FILENAME = os.path.join(os.path.dirname(__file__), "data", "2jyf.cif.gz")
+
+def test_get_model():
+    df = PandasMmcif().read_mmcif(TESTDATA_FILENAME)
+    MODEL_INDEX = 1
+    new_df = df.get_model(MODEL_INDEX)
+    print(df)
+    assert new_df.df["ATOM"]["pdbx_PDB_model_num"].all() == MODEL_INDEX
+
+
+def test_get_models():
+    df = PandasMmcif().read_mmcif(TESTDATA_FILENAME)
+    MODEL_INDICES = [1, 3, 5]
+
+    new_df = df.get_models(MODEL_INDICES)
+    assert new_df.df["ATOM"]["pdbx_PDB_model_num"].all() in MODEL_INDICES
